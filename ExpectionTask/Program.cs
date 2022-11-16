6643403c11b3c586ef29927bf15a28df80eedb21
gemma <gemma@DESKTOP-2Q2G917>
2022-11-16 10:03:33 +0000

    Add project files.

---
+++ ExpectionTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExpectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3000;
+            int num2 = 0;
+
+            try
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divison of " + num2 + " by 0");
+            }
+        }
+    }
+}
